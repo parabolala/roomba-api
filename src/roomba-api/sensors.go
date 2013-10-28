20d9c990c5a3036f67221be68eebcbff1506ff5e
Ievgen Varavva <fuzzy.parabola@gmail.com>
2013-10-28 10:20:40 +0000

    SensorList handler.

--- src/roomba-api/sensors.go
+++ src/roomba-api/sensors.go
@@ -42,3 +42,41 @@ func (server *RoombaServer) GetSensor(w *rest.ResponseWriter, req *rest.Request)
 		Value: sensor_data}
 	w.WriteJson(&response)
 }
+
+type GetSensorsResponse struct {
+	Status
+	Values [][]byte `json:"values"`
+}
+
+func (server *RoombaServer) GetSensors(w *rest.ResponseWriter, req *rest.Request) {
+	conn, err := server.getConnOrWriteError(w, req)
+	if err != nil {
+		return
+	}
+
+	req.ParseForm()
+	packet_id_strs := req.Form["packet_id"]
+
+	packet_ids := make([]byte, 0, len(packet_id_strs))
+	for _, packet_id_str := range packet_id_strs {
+		packet_id, err := strconv.ParseUint(packet_id_str, 10, 8)
+		if err != nil {
+			Error(w, "bad packet id requested: "+packet_id_str,
+				http.StatusNotFound)
+			return
+		}
+		packet_ids = append(packet_ids, byte(packet_id))
+	}
+
+	sensor_data, err := conn.Roomba.QueryList(packet_ids)
+
+	if err != nil {
+		Error(w, "error reading sensors data: "+err.Error(),
+			http.StatusInternalServerError)
+		return
+	}
+
+	response := GetSensorsResponse{Status: Status{"ok"},
+		Values: sensor_data}
+	w.WriteJson(&response)
+}
--- src/roomba-api/sensors_test.go
+++ src/roomba-api/sensors_test.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/url"
 	"roomba"
+	"strconv"
 	"testing"
 )
 
@@ -68,5 +70,57 @@ func TestBadSensorURL(t *testing.T) {
 
 	if code != http.StatusNotFound {
 		t.Errorf("code != 404")
+	}
+}
+
+func TestSensorList(t *testing.T) {
+	server := MakeServer()
+
+	handler := MakeHttpHandlerForServer(server)
+	code, body := GetResponse(handler, "POST", "/ports/"+DUMMY_PORT_NAME)
+
+	if code != 200 {
+		t.Errorf("failed acquiring dummy connection")
+	}
+
+	port_resp := PortPostResponse{}
+	json.Unmarshal(body, &port_resp)
+	conn_id := port_resp.ConnectionId
+	defer ClearDummyRoomba()
+	url_ := fmt.Sprintf("/connection/%d/sensor/list?", conn_id)
+
+	requested_sensors := []byte{roomba.SENSOR_DISTANCE,
+		roomba.SENSOR_WALL}
+	expected_values := [][]byte{{10, 20},
+		{35}}
+
+	qs := url.Values{}
+	for i, packet_data := range expected_values {
+		qs.Add("packet_id", strconv.Itoa(int(requested_sensors[i])))
+		MakeDummyRoomba().S.(*roomba.CloseableRWBuffer).WriteReadBuffer(packet_data)
+	}
+
+	url_ += qs.Encode()
+
+	code, body = GetResponse(handler, "GET", url_)
+	resp := GetSensorsResponse{}
+	json.Unmarshal(body, &resp)
+
+	if resp.Status.Status != "ok" {
+		t.Errorf("status != ok")
+	}
+
+	if len(resp.Values) != len(expected_values) {
+		t.Errorf("returned value len(%v) != expected output len(%v)", resp.Values, expected_values)
+	}
+	for i, packet_data := range resp.Values {
+		if len(packet_data) != len(expected_values[i]) {
+			t.Errorf("returned len for packet_id=%d data: %d != expected len %d", requested_sensors[i], len(resp.Values[i]), len(expected_values[i]))
+		}
+		for j, packet_byte := range resp.Values[i] {
+			if packet_byte != expected_values[i][j] {
+				t.Errorf("returned datafor packet=%d %v!=expected %v in byte %d", requested_sensors[i], resp.Values[i], expected_values[i], j)
+			}
+		}
 	}
 }
